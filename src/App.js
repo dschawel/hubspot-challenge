e79c137aa0cfa6cbba8f3c923e82340841beb122
dschawel <david.schawel@gmail.com>
2020-08-06 15:43:34 -0700

    working on the post route

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import './App.css';
 function App() {
 
   let [data, setData] = useState({ partners: [] })
+  let [firstName, setFirstName] = useState('')
+  let [lastName, setLastName] = useState('')
+  let [email, setEmail] = useState('')
+  let [country, setCountry] = useState('')
+  let [available, setAvailable] = useState('')
 
   // API call via useEffect
   useEffect(() => {
@@ -19,16 +24,41 @@ function App() {
     getData()
   }, []) // Onload only - just empty array
 
-
+const handleSubmit = e => {
+  e.preventDefault()
+  let data = {
+    firstName,
+    lastName,
+    email,
+    country,
+    available
+  }
+  fetch('https://candidate.hubteam.com/candidateTest/v3/problem/result?userKey=52bd2af9be7e3ae4e73d12de6325', {
+    method: 'POST',
+    body: JSON.stringify(data),
+    headers: {
+      'Content-type': 'application/json',
+    }
+  })
+  .then(response => response.json())
+  .then (result => {
+    setFirstName('')
+    setLastName('')
+    setEmail('')
+    setCountry('')
+    setAvailable('')
+  })
+  .catch(err => {
+    console.log('Error', err)
+  })
+}
 // NEED SOMEWAY TO FILTER THROUGH THE LIST OF AVAILABLE DATES
-  // let available
-  // if (data.partners.availableDates) {
-  //   available = data.partners.availableDates.filter(date => {
+  // let available = data.partners.filter(date => {
   //     return (
   //       <p>{date}</p>
   //     )
   //   })
-  // }
+  
 
   // Format the data for the results
   let dates = data.partners.map((p, i) => {
@@ -62,6 +92,21 @@ function App() {
         <div>
           {dates}
           {/* {available} */}
+        </div>
+        <div>
+          <form>
+            <lable>First Name</lable>
+            <input name={firstName} onChange={e => setFirstName(e.target.value)} />
+            <lable>Last Name</lable>
+            <input name={lastName} onChange={e => setLastName(e.target.value)} />
+            <lable>Email</lable>
+            <input name={email} onChange={e => setEmail(e.target.value)} />
+            <lable>Country</lable>
+            <input name={country} onChange={e => setCountry(e.target.value)} />
+            <lable>Available Dates</lable>
+            <input name={available} onChange={e => setAvailable(e.target.value)} />
+            <button type="submit" onClick={handleSubmit}>Submit</button>
+          </form>
         </div>
     </div>
   );
